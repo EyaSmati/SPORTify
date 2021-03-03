748b271f5f95d1d522180648f117e1e3b426f09b
YasmineGrati <79474408+YasmineGrati@users.noreply.github.com>
2021-03-03 21:59:35 +0100

    Add files via upload

---
+++ Gestion de Calendrier/SPORTify/src/com/esprit/test/MainProg.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.test;
+
+
+import com.esprit.models.Calendrier;
+import com.esprit.services.ServiceCalendrier;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MainProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException  {
+
+        ServiceCalendrier calend = new ServiceCalendrier();
+
+//        calend.AjouterCoursDansCalendrier(new Calendrier(1, "fitness", "2021-03-12", "17"));
+//        calend.AjouterCoursDansCalendrier(new Calendrier(2, "aerobique", "2021-03-9", "10"));
+//        calend.AjouterCoursDansCalendrier(new Calendrier(3, "karate", "2021-03-14", "13"));
+//        calend.AjouterCoursDansCalendrier(new Calendrier(4, "yoga", "2021-03-20", "9"));
+//        calend.AjouterCoursDansCalendrier(new Calendrier(5, "gym", "2021-03-23", "11"));
+//        calend.AjouterCoursDansCalendrier(new Calendrier(6, "aerobique", "2021-03-11", "19"));
+//
+//         calend.AjouterCoursDansCalendrier(new Calendrier(7, "judo", "2021-03-06", "15"));
+
+//         calend.supprimerCoursDansCalendrier(new Calendrier(4, "yoga", "2021-03-20", "9"));
+//         calend.supprimerCoursDansCalendrier(new Calendrier(1,"fitness", "2021-03-12", "17"));
+
+
+         
+//          calend.modifierCoursDansCalendrier(new Calendrier(3, "taekwando", "2021-03-08", "16"));
+//          calend.modifierCoursDansCalendrier(new Calendrier(5, "aquagym", "2021-03-24", "14"));
+
+//           calend.rechercherCoursDansCalendrier().forEach(System.out::println); 
+
+
+//          calend.afficherCalendrier().forEach(System.out::println);
+
+
+    }
+
+}
